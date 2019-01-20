388ed3460c048dad46881f10bd6aff1a9254069c
Dimayo <44707838+Dimayo@users.noreply.github.com>
2019-01-21 00:10:33 +0600

    added javascript for smooth scrolling

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('a[href^="#"]').click(function(){
+     var target = $(this).attr('href');
+     $('html, body').animate({
+       scrollTop: $(target).offset().top
+     }, 1000);
+  });
+});

e59577e6e5f4889f44ef61754c0e5719b649c427
Dimayo <44707838+Dimayo@users.noreply.github.com>
2019-02-27 04:11:54 +0600

    Added js animation

---
+++ script.js
@@ -0,0 +1,34 @@
+//Smooth scrolling
+(function navSmoothScrolling() {
+    "use strict";
+    const navLinks = document.querySelectorAll('.nav-item');
+
+    for (let n in navLinks) {
+        if (navLinks.hasOwnProperty(n)) {
+            navLinks[n].addEventListener('click', e => {
+                e.preventDefault();
+                document.querySelector(navLinks[n].hash)
+                    .scrollIntoView({
+                        block: "start",
+                        behavior: "smooth"
+                    });
+            });
+        }
+    }
+})();
+
+//Scroll change
+(function scrollChange() {
+    "use strict";
+
+    const part = document.querySelector('.part');
+
+    window.onscroll = () => {
+        let scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+        scrollPos += window.innerHeight / 2;
+
+        if (part.offsetTop <= scrollPos) {
+            part.classList.add('show');
+        }
+    };
+})();

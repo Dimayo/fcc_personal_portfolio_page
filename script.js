e9dad5100559e9303b3bd7e43304430af9ff4a91
Dimayo <44707838+Dimayo@users.noreply.github.com>
2019-02-27 06:37:21 +0600

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@
 
     window.onscroll = () => {
         let scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
-        scrollPos += window.innerHeight / 2;
+        scrollPos += window.innerHeight / 1.4;
 
         if (part.offsetTop <= scrollPos) {
             part.classList.add('show');
